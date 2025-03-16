f5fb515a0f8a8d6f0488b306372490db82e9d7c5
k21062319 <muhammad.r.rahman@kcl.ac.uk>
2025-03-16 00:09:54 +0000

    Create model for temporary file storage

--- papers/models.py
+++ papers/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class UploadedExamPaper(models.Model):
+    file = models.FileField(upload_to="temp/")  # Files are saved in media/temp/
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file.name

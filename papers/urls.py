ebd107ce29c68a9bca13d2e8da6c2f2d4567e633
k21062319 <muhammad.r.rahman@kcl.ac.uk>
2025-03-16 00:28:58 +0000

    add url for generated paper

--- papers/urls.py
+++ papers/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import signup, login, dashboard, logout, paper_builder
+from .views import signup, login, dashboard, logout, paper_builder, generated_paper
 
 urlpatterns = [
     path("signup/", signup, name="signup"),
@@ -7,4 +7,5 @@ urlpatterns = [
     path("dashboard/", dashboard, name="dashboard"),
     path("logout/", logout, name="logout"),
     path("paper_builder/", paper_builder, name="paper_builder"),
+    path("generated_paper/", generated_paper, name="generated_paper"),
 ]
